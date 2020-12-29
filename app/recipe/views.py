ea100a1ce84f98f3c568df39320a8cbb4c59b0cc
germanvglz <12211479verdozagerman@gmail.com>
2020-12-29 21:59:24 +0000

    Ingredient manage Endpoint

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,7 +3,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from recipe import serializers
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 
 class TagViewSet(viewsets.GenericViewSet,
@@ -22,3 +22,17 @@ class TagViewSet(viewsets.GenericViewSet,
     def perform_create(self, serializer):
         """Create a new tag and assign to user"""
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+    """Manage ingredients in database"""
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+    queryset = Ingredient.objects.all().order_by('-name')
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """Return tags only for current authenticated user"""
+        return self.queryset.filter(user=self.request.user)
